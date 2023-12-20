526327c58cfc2008f35cc4f1cf4314d526a5fe43
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-12-20 16:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
+import os
 import pandas as pd
 from flask import Flask, render_template
-from flask_restful import Api,Resource
+from flask_cors import CORS
+from flask_restful import Api, Resource
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("cred1.json", scope)
-# creds = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
 client = gspread.authorize(creds)
 
 sheets_id = '1IVBLRJCX7Fr79PP7_Tc_o9Ml0g9Uv2Q6JS3dqrSqeXY'
-# sheets_id = '1mDNqcbqkesrArQd2IPC_tmIkjXwAbk5ioj8268hCHns'
 sheet = client.open_by_key(sheets_id).sheet1
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/1IVBLRJCX7Fr79PP7_Tc_o9Ml0g9Uv2Q6JS3dqrSqeXY/export?format=csv")
-# df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheets_id}/export?format=csv")
 df = df[1:]
-print(df)
-# records =  df.to_dict(orient='records')
+
 def get_records():
     df = pd.DataFrame(sheet.get_all_records())
     records = df.to_dict(orient='records')
     return records
 
-# instantiate flask
-app =  Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-#init api obj
 api = Api(app)
-
 
 @app.route('/')
 def index():
@@ -48,7 +44,7 @@ def fun2():
 @app.route('/leak.html')
 def fun4():
     return render_template('leak.html')
-#api req
+
 class All(Resource):
     def get(self):
         return get_records()
@@ -63,10 +59,10 @@ class Time(Resource):
         records = get_records()
         return [i for i in records if i['Time'].lower().startswith(val.lower())]
 
-# reg api resources
-api.add_resource(All,'/api/')
-api.add_resource(Flow,'/api/flow/<string:val>')
-api.add_resource(Time,'/api/time/<string:val>')
+api.add_resource(All, '/api/')
+api.add_resource(Flow, '/api/flow/<string:val>')
+api.add_resource(Time, '/api/time/<string:val>')
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port, debug=True)
