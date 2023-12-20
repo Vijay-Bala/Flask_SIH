7a2e40b2f90792bfb87feeb2382d7b0db7ef121f
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-12-20 16:11:58 +0530

    Update api_req.py

--- api_req.py
+++ api_req.py
@@ -1,7 +1,7 @@
 import requests
 
-req_f1 = requests.get(url='http://127.0.0.1:5000/api/flow/f1')
-req_f2 = requests.get(url='http://127.0.0.1:5000/api/flow/f2')
+req_f1 = requests.get(url='http://44.226.145.213:5000/api/flow/f1')
+req_f2 = requests.get(url='http://44.226.145.213:5000/api/flow/f2')
 dif = req_f1.json()[-1]['value']-req_f2.json()[-1]['value'] 
 print(req_f1.json()[-1])
 print(req_f2.json()[-1])
